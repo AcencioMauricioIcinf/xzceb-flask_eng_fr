8064c2d32b652bc47c7272f8182f6874593168d6
Mauricio Acencio <acencio.mauricio.icinf@gmail.com>
2023-05-02 20:37:05 -0400

    Added remaining unit tests

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -6,11 +6,15 @@ class TestEnFrTranslation(TestCase):
         self.assertRaises(ValueError, english_to_french, None)
     def test2(self):
         self.assertEqual(english_to_french('Hello'), 'Bonjour')
+    def test3(self):
+        self.assertNotEqual(english_to_french('Hello'), 'Bonjour!')
 
 class TestFrEnTranslation(TestCase):
     def test1(self):
         self.assertRaises(ValueError, french_to_english, None)
     def test2(self):
         self.assertEqual(french_to_english('Bonjour'), 'Hello')
+    def test3(self):
+        self.assertNotEqual(french_to_english('Bonjour'), 'Hello!')
 
 main()
